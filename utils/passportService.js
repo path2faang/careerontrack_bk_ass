3f6be43eeae30d8495e81eed7d597fb5b28fe5f4
Ishmael Destiny Onwe <theimpactindex@gmail.com>
2024-10-30 13:55:10 +0100

    added auth display check

--- utils/passportService.js
+++ utils/passportService.js
@@ -8,7 +8,7 @@ envLoader();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: "https://careerontrack-bk-ass.onrender.com/v1/auth/google/callback"
 }, async (accessToken, refreshToken, profileDetail, cb) => {
     try {
         let account;
